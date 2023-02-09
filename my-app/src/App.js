3f04fc3d576b5c02494d914cc0a13584ad8c29ad
CellerCity <amancha9161@gmail.com>
2023-02-09 22:23:48 +0530

    added login logic --sessions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Videos from './Videos';
@@ -8,15 +8,33 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import { FaUnderline } from 'react-icons/fa';
   
 
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  // return userToken?.token
+  return (userToken === null || userToken === undefined) ? null : userToken.token;
+}
+
+
 function App() {
+  const token = getToken();
+
+  if(!token) {
+    return <SignIn setToken={setToken} />
+  }
+
   return (
     <>
-
       <Router>
         <Routes>
-          <Route exact path='/' element={< SignIn/>}></Route>
+          <Route exact path='/' element={< Videos/>}></Route>
           <Route exact path='/sign_in' element={< SignIn/>}></Route>
           <Route exact path='/sign_up' element={< SignUp/>}></Route>
           <Route exact path='/videos' element={< Videos />}></Route>
