a12ba1fcb2cef5e3e902f822f52017471e10bbdf
CellerCity <amancha9161@gmail.com>
2023-03-08 02:15:22 +0530

    Youtube clone v1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,45 +3,33 @@ import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Videos from './Videos';
 import Puzzle8 from './8Puzzle';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import { FaUnderline } from 'react-icons/fa';
-  
-
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  // return userToken?.token
-  return (userToken === null || userToken === undefined) ? null : userToken.token;
-}
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
+// import { FaUnderline } from 'react-icons/fa'; 
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
 function App() {
-  const token = getToken();
-
-  if(!token) {
-    return <SignIn setToken={setToken} />
-  }
 
   return (
     <>
-      <Router>
+      <BrowserRouter>
+      <Box sx = {{ backgroundColor: '#FFF' }}>
+        <Navbar/>
         <Routes>
-          <Route exact path='/' element={< Videos/>}></Route>
+          <Route path='/' exact element={<Feed /> } />
+          <Route path='/video/:id' exact element={<VideoDetail /> } />
+          <Route path='/channel/:id' exact element={<ChannelDetail /> } />
+          <Route path='/search/:searchTerm' exact element={<SearchFeed /> } />
+          
           <Route exact path='/sign_in' element={< SignIn/>}></Route>
           <Route exact path='/sign_up' element={< SignUp/>}></Route>
           <Route exact path='/videos' element={< Videos />}></Route>
           <Route exact path='/8Puzzle' element={< Puzzle8 />}></Route>
           <Route exact path='/blockDodge' element={< Videos />}></Route>
         </Routes>
-      </Router>
+
+      </Box>
+      </BrowserRouter>
     </>
   );
 }
